5fc5bbefde5062737d1ac1d9aa01ef81e9fe8d64
Jiyaul Haque <71638518+jiyaul504@users.noreply.github.com>
2023-12-03 15:17:10 -0800

    Add files via upload

---
+++ Register.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentFormCsv.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+}
