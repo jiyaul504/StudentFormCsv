5bdf6686a70cbea57788b5c92c5e931d2d19574d
Jiyaul Haque <71638518+jiyaul504@users.noreply.github.com>
2023-12-03 15:17:47 -0800

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentFormCsv.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate { get; set; }
+
+
+
+
+    }
+
+}
